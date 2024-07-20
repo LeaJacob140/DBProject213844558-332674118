39c9b8400ed16acb9bbf63eadc358ae69de4e97d
LeaJacob140 <128721258+LeaJacob140@users.noreply.github.com>
2024-07-20 22:41:30 +0300

    Delete הכנסת נתונים לאחר האינטגרציה directory

--- הכנסת נתונים לאחר האינטגרציה/InsertToMaintenAn.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO MainTenan (idmaintenance, itemId) VALUES (83, 570);
-INSERT INTO MainTenan (idmaintenance, itemId) VALUES (84, 572);
-INSERT INTO MainTenan (idmaintenance, itemId) VALUES (85, 573);
